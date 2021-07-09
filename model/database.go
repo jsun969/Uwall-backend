38aac2bff6c78cf2aa9a3fce9f93e013bd458afb
jsun969 <i@jsun969.cn>
2021-07-09 10:42:26 +0800

    :bug: Give message id when get messages

--- model/database.go
+++ model/database.go
@@ -1,19 +1,25 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Message struct {
-	gorm.Model `json:"-"`
-	Type       string    `json:"type"`
-	FromName   string    `json:"fromName,omitempty"`
-	FromSex    uint      `json:"fromSex,omitempty"`
-	ToName     string    `json:"toName,omitempty"`
-	ToSex      uint      `json:"toSex,omitempty"`
-	Message    string    `json:"message"`
-	Anonymous  bool      `json:"anonymous,omitempty"`
-	ImageUrl   string    `json:"imageUrl,omitempty"`
-	Comments   []Comment `json:"comments,omitempty"`
-	Status     bool      `json:"-"`
+	ID        uint           `gorm:"primarykey"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+	Type      string         `json:"type"`
+	FromName  string         `json:"fromName,omitempty"`
+	FromSex   uint           `json:"fromSex,omitempty"`
+	ToName    string         `json:"toName,omitempty"`
+	ToSex     uint           `json:"toSex,omitempty"`
+	Message   string         `json:"message"`
+	Anonymous bool           `json:"anonymous,omitempty"`
+	ImageUrl  string         `json:"imageUrl,omitempty"`
+	Comments  []Comment      `json:"comments,omitempty"`
+	Status    bool           `json:"-"`
 }
 
 type Comment struct {
