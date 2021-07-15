25fc69682cde05693fb31f6fa589d251d107c7e9
jsun969 <i@jsun969.cn>
2021-07-15 15:58:54 +0800

    :children_crossing: Sort results by create time

--- api/get_messages.go
+++ api/get_messages.go
@@ -4,6 +4,7 @@ import (
 	"Uwall/database"
 	"Uwall/model"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +13,9 @@ func GetMessages(c *gin.Context) {
 	limit, _ := strconv.Atoi(c.Query("limit"))
 	offset, _ := strconv.Atoi(c.Query("offset"))
 	var messages []model.Message
-	if result := database.Db.Model(&model.Message{}).Preload("Comments", "status = ?", "1").Where("status = ?", "1").Limit(limit).Offset(offset).Find(&messages); result.Error != nil {
+	if result := database.Db.Model(&model.Message{}).Preload("Comments", func(db *gorm.DB) *gorm.DB {
+		return db.Where("status = ?", 1).Order("created_at desc")
+	}).Where("status = ?", "1").Order("created_at desc").Limit(limit).Offset(offset).Find(&messages); result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error})
 		return
 	}
--- api/get_messages_with_type.go
+++ api/get_messages_with_type.go
@@ -4,6 +4,7 @@ import (
 	"Uwall/database"
 	"Uwall/model"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
@@ -13,7 +14,9 @@ func GetMessagesWithType(c *gin.Context) {
 	limit, _ := strconv.Atoi(c.Query("limit"))
 	offset, _ := strconv.Atoi(c.Query("offset"))
 	var messages []model.Message
-	if result := database.Db.Model(&model.Message{}).Preload("Comments", "status = ?", "1").Where("status = ? AND type = ?", "1", messageType).Limit(limit).Offset(offset).Find(&messages); result.Error != nil {
+	if result := database.Db.Model(&model.Message{}).Preload("Comments", func(db *gorm.DB) *gorm.DB {
+		return db.Where("status = ?", 1).Order("created_at desc")
+	}).Where("status = ? AND type = ?", "1", messageType).Order("created_at desc").Limit(limit).Offset(offset).Find(&messages); result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error})
 		return
 	}
