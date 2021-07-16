3510d09f2a473b5ce64e998f64526e4c76a0977e
jsun969 <i@jsun969.cn>
2021-07-16 14:35:53 +0800

    :children_crossing: Modify get param to page

--- api/get_messages_with_type.go
+++ api/get_messages_with_type.go
@@ -11,12 +11,11 @@ import (
 
 func GetMessagesWithType(c *gin.Context) {
 	messageType := c.Param("type")
-	limit, _ := strconv.Atoi(c.Query("limit"))
-	offset, _ := strconv.Atoi(c.Query("offset"))
+	page, _ := strconv.Atoi(c.Query("page"))
 	var messages []model.Message
 	if result := database.Db.Model(&model.Message{}).Preload("Comments", func(db *gorm.DB) *gorm.DB {
 		return db.Where("status = ?", 1).Order("created_at desc")
-	}).Where("status = ? AND type = ?", "1", messageType).Order("created_at desc").Limit(limit).Offset(offset).Find(&messages); result.Error != nil {
+	}).Where("status = ? AND type = ?", "1", messageType).Order("created_at desc").Limit(10).Offset(page * 10).Find(&messages); result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error})
 		return
 	}
