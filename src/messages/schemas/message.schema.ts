397685c619cc1d7c16c5ccca4585278900094f76
Justin Sun <i@jsun969.cn>
2022-01-14 20:53:01 +0800

    :card_file_box: set property requirement

--- src/messages/schemas/message.schema.ts
+++ src/messages/schemas/message.schema.ts
@@ -5,20 +5,20 @@ export type MessageDocument = Message & Document;
 
 @Schema({ timestamps: true })
 export class Message extends Document {
-  @Prop()
-  type: 'love' | 'complaint' | 'help' | 'notice' | 'expand';
-  @Prop()
-  fromName: string;
+  @Prop({ required: true })
+  type!: 'love' | 'complaint' | 'help' | 'notice' | 'expand';
+  @Prop({ required: true })
+  fromName!: string;
   @Prop()
   fromGender?: boolean;
   @Prop()
   toName?: string;
   @Prop()
   toGender?: boolean;
-  @Prop()
-  content: string;
-  @Prop()
-  anonymous: boolean;
+  @Prop({ required: true })
+  content!: string;
+  @Prop({ required: true })
+  anonymous!: boolean;
   @Prop({ default: 0 })
   likes: number;
   @Prop({ default: false })
